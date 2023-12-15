ef17dc76ece221b4e7cfad6450b09cb2c156ca66
Younghoon Ahn <ahnqwerty@gmail.com>
2023-12-15 22:36:49 +0900

    refactor: 코드 리팩토링
    MVC 구조 구현

---
+++ src/constants/constants.js
@@ -0,0 +1,28 @@
+const ERROR_MESSAGE = {
+    
+    WRONG_NUMBER_TYPE: `[ERROR] 숫자 형식을 다시 확인하세요.`,
+
+    WRONG_NUMBER_DIGIT: `[ERROR] 숫자는 3자리의 양수로 입력해야 합니다.`,
+
+    WRONG_NUMBER_SIGN: `[ERROR] 숫자는 양수로 입력해야 합니다`
+}
+
+const INPUTVIEW_MESSAGE = {
+
+    GET_NUMBER: `숫자를 입력해주세요 : `,
+}
+
+const OUTPUTVIEW_MESSAGE = {
+
+    GAME_START: `숫자 야구 게임을 시작합니다.`,
+
+    BALL: `볼`,
+
+    STRIKE: `스트라이크`,
+
+    NOTHING: `낫싱`,
+
+    GAME_END: `3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+
+    ASK_RETRY: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`
+}
